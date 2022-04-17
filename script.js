1fb315553a73a28176fbd1cb47070a5395ec7a90
Timofey <timofeyvalerievich@gmail.com>
2022-04-17 17:25:30 +0300

    delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function points(games) {
-    let count = 0;
-    games.forEach(item => {
-        if (item[0] > item[2]) count += 3;
-        if (item[0] == item[2]) count += 1;
-        if (item[0] < item[2]) count += 0;
-    });
-    return count;
-}
