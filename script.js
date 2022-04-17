18e4b92cecfd47de05c5da1092fdcf8dc98f4a22
Timofey <timofeyvalerievich@gmail.com>
2022-04-17 17:11:05 +0300

    Total amount of points

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function multiply(a, b) {
-    return a * b;
+function points(games) {
+    let count = 0;
+    games.forEach(item => {
+        if (item[0] > item[2]) count += 3;
+        if (item[0] == item[2]) count += 1;
+        if (item[0] < item[2]) count += 0;
+    });
+    return count;
 }
