9fe2ef219ecf78e6512767ec441a599fb1e49327
Timofey <timofeyvalerievich@gmail.com>
2022-07-22 23:05:53 +0300

    emove the minimum

---
+++ Remove the minimum.js
@@ -0,0 +1,42 @@
+// Task
+
+// Given an array of integers, remove the smallest value. 
+// Do not mutate the original array/list. If there are multiple 
+// elements with the same value, remove the one with a lower index. 
+// If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+
+// Examples
+// * Input: [1,2,3,4,5], output= [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+let arr = [2,2,1,2,1];
+
+function removeSmallest(numbers) {
+    let arr;
+    const min = Math.min(...numbers);
+          firstNum = numbers.indexOf(Math.min(...numbers));
+          secNum = numbers.lastIndexOf(Math.min(...numbers));
+    if (typeof(numbers) == 'string') {
+        arr = numbers.split(',')
+    } else {
+        arr = numbers
+    }
+
+    if (arr.lenght == 0) {
+        return []
+    } else if (firstNum != secNum) {
+        let newArr = arr.filter((num) => {return num != min});
+        newArr.push(arr[firstNum]);
+        return newArr
+    } else {
+        return  arr.filter((num) => {return num != min})
+    }
+}
+
+
+ console.log(removeSmallest(arr))
+
+
